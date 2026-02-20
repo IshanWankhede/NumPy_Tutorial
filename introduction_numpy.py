66cd1ab0ce06af0a7bde7095095753c8bb037223
IshanWankhede <ishan.wankhede7@gmail.com>
2026-02-20 21:23:51 +0530

    Adding New Files

---
+++ introduction_numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# print(np.__version__)
+
+# my_list = [1,2,3,4]  # list is mutable so we can perfor direct oprations on it and it will change 
+
+# my_list = my_list * 2
+
+# print(my_list)
+
+# instaed of using python list we use numpy array for faster use
+
+array = np.array([1,2,3,4])
+
+array = array * 2  # works well not like list of python
+
+print(array)
+# print(type(array))  <class 'numpy.ndarray'> nd meaning n-dimensional array
