6b47322808a5d114a15e271e37053aeb015e0af1
IshanWankhede <ishan.wankhede7@gmail.com>
2026-02-22 11:06:51 +0530

    adding arithimetic opretions

---
+++ arithimetic.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+#Scalar arithimetic 
+
+array = np.array([1, 2, 3])
+
+print(array + 1)   #[2 3 4]
+print(array - 2)   #[-1  0  1]
+print(array * 3)   #[3 6 9]
+print(array / 4)   #[0.25 0.5  0.75]
+print(array ** 5)  #[  1  32 243]
+
+#Vectorized Math Functions 
+
+array = np.array([1.01, 2.5, 3.99])
+
+print(np.sqrt(array))  #[1.         1.41421356 1.73205081]
+
+print(np.round(array))  #[1. 2. 4.]
+print(np.ceil(array))  #[2. 3. 4.]  round up
+print(np.floor(array)) #[1. 2. 3.]  round down
+
+print(np.pi)  # return the value of pi
+
+
+#EXERCISE
+
+radii = np.array([1, 2, 3])
